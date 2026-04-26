d255dd988e8206032e6fcec862b9b1030950abed
Fernando Henrique <xnandoh@gmail.com>
2026-04-26 13:38:54 -0300

    qwe

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation, useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 import Navigation from './components/Navigation'
 import Home from './pages/Home'
 import Test from './pages/Test'
@@ -8,16 +9,40 @@ import Ranking from './pages/Ranking'
 function App() {
   return (
     <Router basename="/descubra-sobre-voce">
-      <div className="min-h-screen bg-gradient-to-br from-purple-50 via-blue-50 to-pink-50 dark:from-gray-900 dark:via-purple-900 dark:to-gray-900">
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/teste/:id" element={<Test />} />
-          <Route path="/perfil" element={<Profile />} />
-          <Route path="/ranking" element={<Ranking />} />
-        </Routes>
-      </div>
+      <AppContent />
     </Router>
+  )
+}
+
+function AppContent() {
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    // Lida com redirecionamento do 404.html do GitHub Pages
+    const searchParams = new URLSearchParams(location.search)
+    const redirectPath = searchParams.get('p')
+    const redirectQuery = searchParams.get('q')
+
+    if (redirectPath) {
+      let newPath = '/' + redirectPath.replace(/~/g, '&')
+      if (redirectQuery) {
+        newPath += '?' + redirectQuery.replace(/~/g, '&')
+      }
+      navigate(newPath + location.hash, { replace: true })
+    }
+  }, [location, navigate])
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-purple-50 via-blue-50 to-pink-50 dark:from-gray-900 dark:via-purple-900 dark:to-gray-900">
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/teste/:id" element={<Test />} />
+        <Route path="/perfil" element={<Profile />} />
+        <Route path="/ranking" element={<Ranking />} />
+      </Routes>
+    </div>
   )
 }
 
